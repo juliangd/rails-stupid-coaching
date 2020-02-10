0fe5ff9c0a4feca22f4642a66cf9a136661b7c8d
Julian Giertsen da Cunha <juliangdacunha@gmail.com>
2020-02-10 15:09:37 +0100

    controller and view in questions

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,5 +3,23 @@ class QuestionsController < ApplicationController
   end
 
   def answer
+    @question = params[:ask]
+    @answer = stupid_coach(@question)
   end
+
+
+  def stupid_coach(input)
+    if input == "I am going to work"
+      "Great!"
+    elsif input.include? "?"
+      "Silly question, get dressed and go to work!"
+    else
+      "I don't care, get dressed and go to work!"
+    end
+  end
+
 end
+
+# The answer.html.erb will display the question you ask your coach as well as his answer.
+# The controller will need to read the question from params and compute an instance variable @answer for the view to display.
+#  Here are two requests that you should be able to handle:
