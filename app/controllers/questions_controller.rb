ca661ec992470c15d0bdb6fd7a4addcc3af3b6d1
Julian Giertsen da Cunha <juliangdacunha@gmail.com>
2020-02-10 15:51:26 +0100

    added style

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,21 +3,19 @@ class QuestionsController < ApplicationController
   end
 
   def answer
-    @question = params[:ask]
+    @question = params[:question]
     @answer = stupid_coach(@question)
   end
 
-
   def stupid_coach(input)
-    if input == "I am going to work"
-      "Great!"
-    elsif input.include? "?"
-      "Silly question, get dressed and go to work!"
+    if input == 'I am going to work'
+      'Great!'
+    elsif input.include? '?'
+      'Silly question, get dressed and go to work!'
     else
       "I don't care, get dressed and go to work!"
     end
   end
-
 end
 
 # The answer.html.erb will display the question you ask your coach as well as his answer.
